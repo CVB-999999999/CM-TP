8e7245a667954ce3ac1bbb20978040b0ce551b92
TatianaFaria <79472256+TatianaFaria@users.noreply.github.com>
2022-05-04 18:32:23 +0100

    login e primeira
    ainda não finalizado

---
+++ app/src/main/java/com/example/bd/PrimeiraActivity.kt
@@ -0,0 +1,21 @@
+package com.example.bd
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.app.ActionBar
+
+class PrimeiraActivity : AppCompatActivity() {
+
+    //Action Bar
+    private lateinit var actionBar: ActionBar
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_primeira)
+
+        //configure actionbar
+        actionBar = supportActionBar!!
+        actionBar.title = "Rental Student"
+    }
+}
