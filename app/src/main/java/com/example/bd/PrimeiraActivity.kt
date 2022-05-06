40c2ca4ea55bdd471627417b27aee5030a6e4fa6
pdanielferreira <pedro.daniel.duarte@hotmail.com>
2022-05-06 12:33:48 +0100

    btn acessos

--- app/src/main/java/com/example/bd/PrimeiraActivity.kt
+++ app/src/main/java/com/example/bd/PrimeiraActivity.kt
@@ -1,13 +1,17 @@
 package com.example.bd
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.app.ActionBar
+import com.example.bd.databinding.ActivityPrimeiraBinding
 
 class PrimeiraActivity : AppCompatActivity() {
 
     //Action Bar
     private lateinit var actionBar: ActionBar
+
+    private lateinit var binding: ActivityPrimeiraBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,5 +21,14 @@ class PrimeiraActivity : AppCompatActivity() {
         //configure actionbar
        // actionBar = supportActionBar!!
        // actionBar.title = "Rental Student"
+
+        binding.btnLogin.setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
+
+        binding.btnRegistar.setOnClickListener {
+            startActivity(Intent(this, SignUp::class.java))
+        }
+
     }
 }
