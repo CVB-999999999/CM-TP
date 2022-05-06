5c29ab362ab91e81cdabcdc41417f7daffb39601
pdanielferreira <pedro.daniel.duarte@hotmail.com>
2022-05-06 12:56:17 +0100

    login na pagina inicial

--- app/src/main/java/com/example/bd/PrimeiraActivity.kt
+++ app/src/main/java/com/example/bd/PrimeiraActivity.kt
@@ -1,17 +1,25 @@
 package com.example.bd
 
+import android.app.ProgressDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.ActionBar
+import com.example.bd.databinding.ActivityLoginBinding
 import com.example.bd.databinding.ActivityPrimeiraBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class PrimeiraActivity : AppCompatActivity() {
 
     //Action Bar
     private lateinit var actionBar: ActionBar
 
+    //viewBinding
+    private lateinit var binding: ActivityPrimeiraBinding
+
+    //Firebase auth
+    private lateinit var firebaseAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +30,25 @@ class PrimeiraActivity : AppCompatActivity() {
        // actionBar = supportActionBar!!
        // actionBar.title = "Rental Student"
 
+        binding = ActivityPrimeiraBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //init FirebaseAuth
+        firebaseAuth = FirebaseAuth.getInstance()
+        checkUser()
+
+
+    }
+
+    private fun checkUser() {
+        //Se já estiver logado vai direto para o perfil
+        //obter o current user (esqueci-me com é a tradução)
+        val firebaseUser = firebaseAuth.currentUser
+        if(firebaseUser != null){
+            //utilizador já logado
+            startActivity(Intent(this, Profile::class.java))
+            finish()
+        }
     }
 
     fun goLogin(view: View) {
