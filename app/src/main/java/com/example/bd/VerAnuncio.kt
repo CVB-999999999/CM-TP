397c48af09469e78a2eb4957ba1b9d2167973d6c
pdanielferreira <pedro.daniel.duarte@hotmail.com>
2022-05-09 17:46:43 +0100

    adicionar aos favoritos
    No ver anúncios, ao clicar no coração já adiciona aos favoritos

--- app/src/main/java/com/example/bd/VerAnuncio.kt
+++ app/src/main/java/com/example/bd/VerAnuncio.kt
@@ -1,21 +1,34 @@
 package com.example.bd
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import com.example.bd.databinding.ActivityProfileBinding
 import com.example.bd.databinding.ActivityVerAnuncioBinding
 import com.example.bd.models.studentList
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 import com.squareup.picasso.Picasso
+import java.util.*
+import kotlin.collections.HashMap
 
 class VerAnuncio : AppCompatActivity() {
 
     private lateinit var binding: ActivityVerAnuncioBinding
+
+    private lateinit var firebaseAuth: FirebaseAuth
+
+    //aramzena o valor se esta ou não nos favoritos, começa como falso
+    private var isInMyFavorite = false
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,13 +38,32 @@ class VerAnuncio : AppCompatActivity() {
 
         val codAnuncio = intent.getStringExtra("codAnuncio")
 
+        firebaseAuth = FirebaseAuth.getInstance()
+        if (firebaseAuth.currentUser != null){
+            checkFav(codAnuncio!!)
+        }
+
         loadAnuncio(codAnuncio!!)
+
+        //Favoritos
+        binding.favorite.setOnClickListener {
+            if (firebaseAuth.currentUser == null){
+                Toast.makeText(this, "Necessita de ter login efetuado!", Toast.LENGTH_SHORT).show()
+            }else{
+                if (isInMyFavorite){
+                    //remove
+                    removeFromFav(codAnuncio)
+                }else{
+                    //adiciona
+                    addFavorite(codAnuncio)
+                }
+            }
+        }
 
         //Ativa o modo imersivo
         window.decorView.apply {
             systemUiVisibility = View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_FULLSCREEN
         }
-
     }
 
     private fun loadAnuncio(codAnuncio: String) {
@@ -130,5 +162,62 @@ class VerAnuncio : AppCompatActivity() {
 
                 }
             })
+    }
+
+    private fun checkFav(codAnuncio: String){
+
+        val rel = FirebaseDatabase.getInstance().getReference("Utilizadores")
+        rel.child(firebaseAuth.uid!!).child("Favoritos").child(codAnuncio)
+            .addValueEventListener(object : ValueEventListener{
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    isInMyFavorite = snapshot.exists()
+
+                    if (isInMyFavorite){
+                        //já existia
+                        binding.favorite.setCompoundDrawablesRelativeWithIntrinsicBounds(0, R.drawable.ic_baseline_favorite_24, 0,0)
+                    }else{
+                        binding.favorite.setCompoundDrawablesRelativeWithIntrinsicBounds(0, R.drawable.ic_outline_favorite_border_24, 0,0)
+                    }
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+
+                }
+
+            })
+    }
+
+    private fun addFavorite(codAnuncio: String){
+
+        val timestamp = System.currentTimeMillis()
+
+        val hashMap = HashMap<String, Any>()
+        hashMap["codAnuncio"] = codAnuncio
+        hashMap["timestamp"] = timestamp
+
+        //guardar na BD
+        val rel = FirebaseDatabase.getInstance().getReference("Utilizadores")
+        rel.child(firebaseAuth.uid!!).child("Favoritos").child(codAnuncio)
+            .setValue(hashMap)
+            .addOnSuccessListener {
+                Toast.makeText(this, "Anuncio adicionado aos favoritos", Toast.LENGTH_SHORT).show()
+            }
+            .addOnFailureListener{
+                Toast.makeText(this, "Erro", Toast.LENGTH_SHORT).show()
+            }
+    }
+
+    private fun removeFromFav(codAnuncio: String){
+
+        val rel = FirebaseDatabase.getInstance().getReference("Utilizadores")
+        rel.child(firebaseAuth.uid!!).child("Favoritos").child(codAnuncio)
+            .removeValue()
+            .addOnSuccessListener {
+                Toast.makeText(this, "Anuncio removido dos favoritos", Toast.LENGTH_SHORT).show()
+            }
+            .addOnFailureListener{
+                Toast.makeText(this, "Erro", Toast.LENGTH_SHORT).show()
+            }
+
     }
 }
